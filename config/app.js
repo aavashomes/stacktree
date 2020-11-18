6904a947f83c37e073b366ef86234c344458dca8
Aditya Mandil <adityamandil317@gmailcom>
2020-11-18 21:44:52 +0530

    First Commit

---
+++ config/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCosWKerHNVXEWGz22sA7pYiMV1mK3kPR0",
+    authDomain: "mystacktree.firebaseapp.com",
+    databaseURL: "https://mystacktree.firebaseio.com",
+    projectId: "mystacktree",
+    storageBucket: "mystacktree.appspot.com",
+    messagingSenderId: "298986481865",
+    appId: "1:298986481865:web:c0bfd4c8c159caea054518"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
